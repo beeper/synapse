db600a73b2b7921d137244635ab127f8ac3b3dcb
Nick Mills-Barrett <nick@beeper.com>
2024-03-08 16:57:42 +0000

    Add Beeper previews

--- synapse/storage/databases/main/__init__.py
+++ synapse/storage/databases/main/__init__.py
@@ -41,6 +41,7 @@ from synapse.types import get_domain_from_id
 
 from .account_data import AccountDataStore
 from .appservice import ApplicationServiceStore, ApplicationServiceTransactionStore
+from .beeper import BeeperStore
 from .cache import CacheInvalidationWorkerStore
 from .censor_events import CensorEventsStore
 from .client_ips import ClientIpWorkerStore
@@ -158,6 +159,7 @@ class DataStore(
     SessionStore,
     TaskSchedulerWorkerStore,
     SlidingSyncStore,
+    BeeperStore,
 ):
     def __init__(
         self,
