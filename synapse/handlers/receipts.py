10f88dc209e01c0acd49a6dd4cbef0cb6be31b01
Nick Mills-Barrett <nick@beeper.com>
2024-03-08 17:01:56 +0000

    Allow clients to add extra content to receipts

--- synapse/handlers/receipts.py
+++ synapse/handlers/receipts.py
@@ -181,6 +181,7 @@ class ReceiptsHandler:
         user_id: UserID,
         event_id: str,
         thread_id: Optional[str],
+        extra_content: Optional[JsonDict] = None,
     ) -> None:
         """Called when a client tells us a local user has read up to the given
         event_id in the room.
@@ -197,7 +198,7 @@ class ReceiptsHandler:
             user_id=user_id.to_string(),
             event_ids=[event_id],
             thread_id=thread_id,
-            data={"ts": int(self.clock.time_msec())},
+            data={"ts": int(self.clock.time_msec()), **(extra_content or {})},
         )
 
         is_new = await self._handle_new_receipts([receipt])
