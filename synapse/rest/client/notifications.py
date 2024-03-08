7daa5d2c7ac7f5c60eb2cbed3f6494c0681c25cc
Nick Mills-Barrett <nick@beeper.com>
2024-03-08 17:01:56 +0000

    Allow clients to add extra content to receipts

--- synapse/rest/client/notifications.py
+++ synapse/rest/client/notifications.py
@@ -70,6 +70,7 @@ class NotificationsServlet(RestServlet):
             [
                 ReceiptTypes.READ,
                 ReceiptTypes.READ_PRIVATE,
+                ReceiptTypes.BEEPER_INBOX_DONE,
             ],
         )
 
