873dade25d56de087e3242bc1ad4edb4d804e4ad
Nick Mills-Barrett <nick@beeper.com>
2024-03-12 17:35:15 +0000

    Add prometheus histograms for room shutdown/delete/purge

--- synapse/handlers/room.py
+++ synapse/handlers/room.py
@@ -25,6 +25,7 @@ import logging
 import math
 import random
 import string
+import time
 from collections import OrderedDict
 from http import HTTPStatus
 from typing import (
@@ -40,6 +41,8 @@ from typing import (
 )
 
 import attr
+from prometheus_client import Histogram
+from typing_extensions import TypedDict
 
 import synapse.events.snapshot
 from synapse.api.constants import (
@@ -102,6 +105,17 @@ logger = logging.getLogger(__name__)
 id_server_scheme = "https://"
 
 FIVE_MINUTES_IN_MS = 5 * 60 * 1000
+
+
+shutdown_time = Histogram("room_shutdown_time", "Time taken to shutdown rooms (sec)")
+shutdown_kick_count = Histogram(
+    "room_shutdown_kick_count",
+    "Number of users successfully kicked while shutting down a room",
+)
+shutdown_failed_kick_count = Histogram(
+    "room_shutdown_failed_kick_count",
+    "Number of users that were failed to be kicked while shutting down a room",
+)
 
 
 @attr.s(slots=True, frozen=True, auto_attribs=True)
@@ -1914,6 +1928,7 @@ class RoomShutdownHandler:
         else:
             logger.info("Shutting down room %r", room_id)
 
+        shutdown_start = time.time()
         users = await self.store.get_local_users_related_to_room(room_id)
         for user_id, membership in users:
             # If the user is not in the room (or is banned), nothing to do.
@@ -1999,5 +2014,10 @@ class RoomShutdownHandler:
             )
         else:
             result["local_aliases"] = []
+
+        shutdown_end = time.time()
+        shutdown_kick_count.observe(len(result["kicked_users"]))
+        shutdown_failed_kick_count.observe(len(result["failed_to_kick_users"]))
+        shutdown_time.observe(shutdown_end - shutdown_start)
 
         return result
