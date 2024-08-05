c8040b3c5be2cec522007ebc65723e48ef8c7653
Nick Mills-Barrett <nick@beeper.com>
2024-08-05 14:55:16 +0100

    Add Beeper notification counts

--- synapse/storage/databases/main/events.py
+++ synapse/storage/databases/main/events.py
@@ -2232,6 +2232,8 @@ class PersistEventsStore:
                 ),
             )
 
+            self.store.beeper_add_notification_counts_txn(txn, notifiable_events)
+
         # Now we delete the staging area for *all* events that were being
         # persisted.
         txn.execute_batch(
