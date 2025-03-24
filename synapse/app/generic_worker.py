0aa77ae3512b393c6a8f264dd388bf1f0260f3be
Nick Mills-Barrett <nick@beeper.com>
2025-03-24 11:03:12 +0000

    Add Beeper previews

--- synapse/app/generic_worker.py
+++ synapse/app/generic_worker.py
@@ -63,6 +63,7 @@ from synapse.storage.databases.main.appservice import (
     ApplicationServiceTransactionWorkerStore,
     ApplicationServiceWorkerStore,
 )
+from synapse.storage.databases.main.beeper import BeeperStore
 from synapse.storage.databases.main.censor_events import CensorEventsStore
 from synapse.storage.databases.main.client_ips import ClientIpWorkerStore
 from synapse.storage.databases.main.delayed_events import DelayedEventsStore
@@ -163,6 +164,7 @@ class GenericWorkerStore(
     ExperimentalFeaturesStore,
     SlidingSyncStore,
     DelayedEventsStore,
+    BeeperStore,
 ):
     # Properties that multiple storage classes define. Tell mypy what the
     # expected type is.
--- synapse/handlers/room.py
+++ synapse/handlers/room.py
@@ -43,7 +43,6 @@ from typing import (
 
 import attr
 from prometheus_client import Histogram
-from typing_extensions import TypedDict
 
 import synapse.events.snapshot
 from synapse.api.constants import (
