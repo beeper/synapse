b78f586de6948524a7687bafc0c2825c1297296d
Nick Mills-Barrett <nick@beeper.com>
2025-03-24 11:16:29 +0000

    Update CI/fix types for recent synapse changes
    Still broken because the opentracing module upstream uses has long been
    retried and setuptools broke it.

--- tests/push/test_push_rule_evaluator.py
+++ tests/push/test_push_rule_evaluator.py
@@ -1004,11 +1004,12 @@ class PushRuleEvaluatorBaseRulesTestCase(unittest.TestCase):
             True,
             reaction_event.room_version.msc3931_push_features,
             True,
+            True,
         )
 
         # Reaction to Brad's message, should be an action for Brad
         actions = dm_evaluator.run(
-            FilteredPushRules(PushRules([]), {}, True, True, True, True),
+            FilteredPushRules(PushRules([]), {}, True, True, True, True, True),
             "@brad:beeper.com",
             "Brad",
         )
@@ -1016,7 +1017,7 @@ class PushRuleEvaluatorBaseRulesTestCase(unittest.TestCase):
 
         # Reaction to Brad's message, should not be an action for Nick
         actions = dm_evaluator.run(
-            FilteredPushRules(PushRules([]), {}, True, True, True, True),
+            FilteredPushRules(PushRules([]), {}, True, True, True, True, True),
             "@nick:beeper.com",
             "Nick",
         )
@@ -1032,17 +1033,18 @@ class PushRuleEvaluatorBaseRulesTestCase(unittest.TestCase):
             True,
             reaction_event.room_version.msc3931_push_features,
             True,
+            True,
         )
 
         # Large rooms should never have emoji reaction notifications
         actions = large_room_evaluator.run(
-            FilteredPushRules(PushRules([]), {}, True, True, True, True),
+            FilteredPushRules(PushRules([]), {}, True, True, True, True, True),
             "@brad:beeper.com",
             "Brad",
         )
         self.assertEqual(actions, [])
         actions = large_room_evaluator.run(
-            FilteredPushRules(PushRules([]), {}, True, True, True, True),
+            FilteredPushRules(PushRules([]), {}, True, True, True, True, True),
             "@nick:beeper.com",
             "Nick",
         )
@@ -1076,10 +1078,11 @@ class PushRuleEvaluatorBaseRulesTestCase(unittest.TestCase):
             True,
             event.room_version.msc3931_push_features,
             True,
+            True,
         )
 
         actions = evaluator.run(
-            FilteredPushRules(PushRules([]), {}, True, True, True, True),
+            FilteredPushRules(PushRules([]), {}, True, True, True, True, True),
             "@brad:beeper.com",
             "Brad Murray",
         )
