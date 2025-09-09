28d83f61e2c5e102ec8070f01ba5311c7a19e9b1
SpiritCroc <dev@spiritcroc.de>
2025-09-09 09:47:31 +0200

    Make changes an opt-in to MSC-4169

--- synapse/rest/client/versions.py
+++ synapse/rest/client/versions.py
@@ -177,6 +177,8 @@ class VersionsRestServlet(RestServlet):
                     "uk.tcpip.msc4133": self.config.experimental.msc4133_enabled,
                     # MSC4155: Invite filtering
                     "org.matrix.msc4155": self.config.experimental.msc4155_enabled,
+                    # MSC4169: Backwards-compatible redaction sending useing `/send`
+                    "com.beeper.msc4169": self.config.experimental.msc4169_enabled,
                 },
             },
         )
