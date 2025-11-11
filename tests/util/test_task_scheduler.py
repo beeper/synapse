f818cacc465653d1380a84893aa3b09f7180cb46
Nick Mills-Barrett <nick@beeper.com>
2025-11-11 16:52:25 +0000

    Temporarily reduce scheduled task load
    - reduce hardcoded task parallelism
    - add hardcoded post task sleep

--- tests/util/test_task_scheduler.py
+++ tests/util/test_task_scheduler.py
@@ -141,12 +141,14 @@ class TestTaskScheduler(HomeserverTestCase):
         self.assertEqual(len(scheduled_tasks), 1)
 
         # The scheduled task should start 0.1s after the first of the active tasks
-        # finishes
-        self.reactor.advance(0.1)
+        # finishes its post-task throttle.
+        self.reactor.advance(TaskScheduler.SLEEP_AFTER_TASK_S)
+        self.reactor.advance(0.2)
         self.assertEqual(len(get_tasks_of_status(TaskStatus.ACTIVE)), 1)
 
-        # ... and should finally complete after another second
-        self.reactor.advance(1)
+        # ... and should finally complete after it runs.
+        self.reactor.advance(TaskScheduler.SLEEP_AFTER_TASK_S)
+        self.reactor.advance(0.2)
         prev_scheduled_task = self.get_success(
             self.task_scheduler.get_task(scheduled_tasks[0].id)
         )
@@ -210,7 +212,8 @@ class TestTaskScheduler(HomeserverTestCase):
         assert task.result and "counter" in task.result
         current_counter = int(task.result["counter"])
 
-        self.reactor.advance(1)
+        self.reactor.advance(TaskScheduler.SLEEP_AFTER_TASK_S)
+        self.reactor.advance(0.2)
 
         task = self.get_success(self.task_scheduler.get_task(task_id))
         assert task is not None
