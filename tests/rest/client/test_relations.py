b64e9e80577fc36f43a9c47541fa11c7ab91ed09
Radon Rosborough <radon@beeper.com>
2026-04-09 14:01:22 -0700

    Fix CI

--- tests/rest/client/test_relations.py
+++ tests/rest/client/test_relations.py
@@ -20,7 +20,7 @@
 #
 
 import urllib.parse
-from typing import Any, Callable, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 from unittest.mock import AsyncMock, patch
 
 from twisted.internet.testing import MemoryReactor
